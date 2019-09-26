d43fe58267a5f46f3c6322042cee7ce2ee342d2c
Peter Faucella <MufasaRips@Peters-MacBook-Air.local>
2019-09-26 17:19:47 -0400

    Fixing forecast return data 2

--- source/utils/forecast.js
+++ source/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lattitude, longitude, callback) => {
         } else if(response.body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            let forecast = `${response.body.daily.data[0].summary}. The high is ${response.body.daily.data[0].temperatureHigh} with a low of ${response.body.daily.data[0].temperatureLow} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain. The U.V index is ${response.body.currently.uvIndex}`
+            let forecast = `${response.body.daily.data[0].summary} The high is ${Math.floor(response.body.daily.data[0].temperatureHigh)} with a low of ${Math.floor(response.body.daily.data[0].temperatureLow)}. It is currently ${Math.floor(response.body.currently.temperature)} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain and the U.V index is ${response.body.currently.uvIndex}`
             callback(undefined, forecast)
         }
     })
